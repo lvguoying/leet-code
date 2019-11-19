25055a169c13ede9b4f7414ce3dae59fcb4ad797
lvguoqing <lvguoqing@daojia-inc.com>
2019-11-19 15:51:21 +0800

    first commit

---
+++ leetCode/src/main/java/com/share/solution/Solution.java
@@ -0,0 +1,11 @@
+package com.share.solution;
+
+/**
+ * @author Lgq create on 2019/11/19
+ **/
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Lee Code");
+    }
+}

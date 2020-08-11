030b8185db2fa61750bfd27498a0b5395c0108c3
lvguoqing <1029839312@qq.com>
2020-08-11 15:15:08 +0800

    keep leaning

---
+++ leetCode/src/main/java/com/share/solution/difficulty/two/MaxProfit.java
@@ -0,0 +1,23 @@
+package com.share.solution.difficulty.two;
+
+/**
+ * 309. 最佳买卖股票时机含冷冻期
+ * 给定一个整数数组，其中第 i 个元素代表了第 i 天的股票价格 。​
+ * <p>
+ * 设计一个算法计算出最大利润。在满足以下约束条件下，你可以尽可能地完成更多的交易（多次买卖一支股票）:
+ * <p>
+ * 你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+ * 卖出股票后，你无法在第二天买入股票 (即冷冻期为 1 天)。
+ *
+ * @author Lgq create on 2020/7/10
+ */
+public class MaxProfit {
+
+
+    public int maxProfit(int[] prices) {
+
+
+        return 11;
+
+    }
+}
